89e313eb57e659f7942ac7c4a75f4c19e3ce8e5d
onesol98 <ruaenddl981028@naver.com>
2022-10-24 00:56:37 +0900

    fix : 더미데이터 수정

--- src/DB/TodoDb.ts
+++ src/DB/TodoDb.ts
@@ -4,7 +4,7 @@ export const TodoList: ITodo[] = [
   {
     id: 0,
     startDate: '2022-10-21',
-    endDate: '2022-10-21',
+    dateTime: '10:10',
     title: '배포하기',
     content: '만들어둔 사이트 배포하는 날',
     type: 2,
@@ -12,7 +12,7 @@ export const TodoList: ITodo[] = [
   {
     id: 1,
     startDate: '2022-10-21',
-    endDate: '2022-10-21',
+    dateTime: '10:10',
     title: '정규회의',
     content: '12C 에서 회의하기',
     type: 1,
@@ -20,7 +20,7 @@ export const TodoList: ITodo[] = [
   {
     id: 2,
     startDate: '2022-10-10',
-    endDate: '2022-10-10',
+    dateTime: '12:10',
     title: '개발하는 날',
     content: '개발',
     type: 2,
@@ -28,9 +28,9 @@ export const TodoList: ITodo[] = [
   {
     id: 3,
     startDate: '2022-10-20',
-    endDate: '2022-10-25',
+    dateTime: '14:23',
     title: '코딩테스트 공부하기',
     content: '개발',
     type: 2,
-  },
+  }
 ]
