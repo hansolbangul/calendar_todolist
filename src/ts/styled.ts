1bc965393a01aa945bceb4865ba5d23194189adc
onesol98 <ruaenddl981028@naver.com>
2022-10-24 00:57:35 +0900

    fix : 스타일 변경

--- src/ts/styled.ts
+++ src/ts/styled.ts
@@ -1,53 +1,34 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 100%;
-  height: 100%;
+export const Center = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
-`;
+`
 
-export const PostForm = styled.div`
-  width: 600px;
-  padding: 1rem;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 1rem;
-  place-items: center;
-`;
-
-export const Flex = styled.div<{ position: string }>`
+export const Container = styled.div`
+  margin-top: 100px;
   display: flex;
-  align-items: center;
-  justify-content: ${(props) => props.position};
+  flex-direction: column;
 `;
 
-export const Title = styled.div`
-  font-size: 1.4rem;
-  padding-bottom: 10px;
-  border-bottom: 1px solid black;
-`;
+export const TitleForm = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 800px;
+`
 
-export const LinkTitle = styled(Link)`
-  font-size: 1.4rem;
-  padding-bottom: 10px;
-  border-bottom: 1px solid black;
-`;
+export const Flex = styled.div`
+  display: flex;
+`
 
-export const Tag = styled.span`
-  padding: 6px 8px;
-  border-radius: 10px;
-  background-color: ${(props) => props.theme.bgColor};
-`;
+export const Div = styled.div``
 
-export const CreateAt = styled.div``;
-
-export const Button = styled.button<{ color: string }>`
-  padding: 6px 8px;
-  background-color: #12b886;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 1rem;
-  color: ${(props) => props.theme.bgColor};
-`;
+export const Color = styled.div<{ color?: string }>`
+  width: 10px;
+  height: 10px;
+  background-color: ${props => props.color};
+`
