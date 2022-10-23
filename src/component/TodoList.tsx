53baf7bdf8a3f5df5b3854c7c4dfe975368ebc2e
onesol98 <ruaenddl981028@naver.com>
2022-10-24 00:55:59 +0900

    feat : 일정 리스트 추가

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -4,13 +4,14 @@ import styled from "styled-components";
 import { isTodoAtom } from "../atoms";
 import { TypeList } from "../DB/TypeDb";
 import eventBus from "../eventBus/bus";
-import { IDate } from "../ts/interface";
+import { IDate, ITodo } from "../ts/interface";
 import { Flex } from "../ts/styled";
+import { Todo } from "./Todo";
 
 interface IProps { item: IDate }
 
 export const TodoList = ({ item }: IProps) => {
-  const [isTodo, setIsTodo] = useRecoilState(isTodoAtom)
+  const [isTodo, setIsTodo] = useRecoilState<ITodo[]>(isTodoAtom)
   const [typeList, setTypeList] = useState(TypeList)
 
   const modal = () => {
@@ -18,7 +19,14 @@ export const TodoList = ({ item }: IProps) => {
   }
 
   const listTodo = useCallback(() => {
+    const tagArr = (isTodo.filter(value => new Date(value.startDate).getTime() === new Date(`${item.year}-${item.month}-${item.date}`).getTime())
+      .sort((a, b) => new Date(`${a.startDate} ${a.dateTime}`).getTime() - new Date(`${b.startDate} ${b.dateTime}`).getTime())
+      .map(_item => {
+        const type = typeList.find(value => value.id === _item.type)
+        return <Todo key={_item.id} color={type?.color} type={type?.title} item={_item} ></Todo>
+      }))
 
+    return tagArr.length > 0 ? tagArr : <NoneData>일정이 없습니다.</NoneData>
 
   }, [item, isTodo])
 
@@ -26,11 +34,18 @@ export const TodoList = ({ item }: IProps) => {
     <ListForm>
       <Btn onClick={modal} >일정 추가 </Btn>
       <ColumnFlex>
-        {/* {listTodo()} */}
+        {listTodo()}
       </ColumnFlex>
     </ListForm>
   )
 }
+
+const NoneData = styled.div`
+  padding: 20px;
+  text-align: center;
+  border: 1px black dashed;
+  border-radius: 10px;
+`
 
 const ListForm = styled.div`
   width: 300px;
@@ -38,16 +53,17 @@ const ListForm = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-  overflow: hidden;
   z-index: 99;
   `
 
 const ColumnFlex = styled(Flex)`
   margin-top: 10px;
-  border: 1px black dashed;
   flex-direction: column;
   flex: 1 1 auto;
   border-radius: 6px;
+  max-height: 500px;
+  
+  overflow: scroll;
 `
 
 
@@ -65,4 +81,6 @@ const Btn = styled.button`
   border-radius: 20px;
   background-color: #f59649;
   color: #fff;
+
+  cursor: pointer;
   `
