7b1f5842e9485a4c41513bdab3846fbca5bd65a5
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2022-10-24 23:28:56 +0900

    feat: 테마 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,21 @@
 import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
-  bgColor: '#121212',
-  textColor: 'white',
-  accentColor: '#1E1E1E',
-  tagBgColor: '#292a2d;',
+  bgColor: 'black',
+  textColor: '#fff',
+  calColor: '#23243B', // 캘린더 색상
+  btnColor: '#2A2B44', // 추가 버튼 색상
+  backColor: '#35364e', // 할일 리스트 색상
+  modalBtnColor: '#2f2f3f95', // 모달 버튼 색상
+  colorOpacity: 1,
 };
 
 export const lightTheme: DefaultTheme = {
   bgColor: '#F8F9FA',
-  textColor: '#121212',
-  accentColor: '#FFFFFF',
-  tagBgColor: '#f8f8f8;',
+  textColor: 'black',
+  calColor: '#fff',
+  btnColor: '#D8D9CF',
+  backColor: '#FAF7F0',
+  modalBtnColor: '#EDDBC0',
+  colorOpacity: 0.6,
 };
