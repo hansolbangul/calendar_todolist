b4fdd178977e63496907e6d391a20a29d9bb0dfa
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2022-10-24 23:31:02 +0900

    feat : 공용 스타일 추가

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,7 +4,10 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     textColor: string;
     bgColor: string;
-    accentColor: string;
-    tagBgColor: string;
+    calColor: string;
+    btnColor: string;
+    backColor: string;
+    modalBtnColor: string;
+    colorOpacity: number
   }
 }
