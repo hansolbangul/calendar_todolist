3f0b65a3e90499905655db46521e55f60ad5d697
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2022-10-25 10:52:59 +0900

    fix : 목업 데이터 수정

--- src/DB/TypeDb.ts
+++ src/DB/TypeDb.ts
@@ -3,17 +3,17 @@ import { IType } from "../ts/interface";
 export const TypeList: IType[] = [
   {
     id: 0,
-    color: '#D84040',
+    color: 'D84040',
     title: '중요',
   },
   {
     id: 1,
-    color: '#48566D',
+    color: '48566D',
     title: '회사',
   },
   {
     id: 2,
-    color: '#00C6E0',
+    color: '00C6E0',
     title: '개인',
   },
 ]
