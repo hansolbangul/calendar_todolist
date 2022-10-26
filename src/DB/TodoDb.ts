346e9d288e00746043dba652b0870aa32e3141ee
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2022-10-26 23:00:47 +0900

    feat : 목업 데이터 추가

--- src/DB/TodoDb.ts
+++ src/DB/TodoDb.ts
@@ -3,30 +3,37 @@ import { ITodo } from "../ts/interface";
 export const TodoList: ITodo[] = [
   {
     id: 0,
-    startDate: '2022-10-21',
-    dateTime: '10:10',
-    title: '배포하기',
+    startDate: '2022-10-28',
+    dateTime: '08:00',
+    title: '내 생일',
     type: 2,
   },
   {
     id: 1,
-    startDate: '2022-10-21',
-    dateTime: '10:10',
-    title: '정규회의',
-    type: 1,
+    startDate: '2022-10-28',
+    dateTime: '14:00',
+    title: '과제 제출날',
+    type: 0,
   },
   {
     id: 2,
-    startDate: '2022-10-10',
+    startDate: '2022-10-23',
     dateTime: '12:10',
     title: '개발하는 날',
     type: 2,
   },
   {
     id: 3,
-    startDate: '2022-10-20',
+    startDate: '2022-10-27',
     dateTime: '14:23',
     title: '코딩테스트 공부하기',
     type: 2,
+  },
+  {
+    id: 4,
+    startDate: '2022-10-21',
+    dateTime: '11:10',
+    title: '회사 머지하기',
+    type: 1,
   }
 ]
